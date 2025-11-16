37bb93dce4b6c7bf5ef6aa07bcfd09e115de42ab
admin <admin@kangaroogd.com.au>
2025-11-16 22:27:55 +0000

    Manual code change

--- src/pages/PriceList.jsx
+++ src/pages/PriceList.jsx
@@ -137,7 +137,7 @@ export default function PriceList() {
             </div>
             {isAdmin &&
             <Button
-              onClick={() => setShowForm(true)} className="bg-[#fae008] text-primary-foreground px-4 py-2 text-sm font-medium rounded-md inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 shadow h-9 hover:bg-orange-700">
+              onClick={() => setShowForm(true)} className="bg-[#fae008] text-slate-950 px-4 py-2 text-sm font-medium rounded-md inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 shadow h-9 hover:bg-orange-700">
 
 
                 <Plus className="w-4 h-4 mr-2" />
