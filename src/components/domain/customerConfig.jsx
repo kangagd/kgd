9f700e2cdfeab698d10ee0dbf57de156b0e1e342
admin <admin@kangaroogd.com.au>
2025-12-01 21:40:13 +0000

    File changes

---
+++ src/components/domain/customerConfig.jsx
@@ -0,0 +1,18 @@
+export const CUSTOMER_TYPE = {
+  OWNER: "Owner",
+  BUILDER: "Builder",
+  REAL_ESTATE_TENANT: "Real Estate - Tenant",
+  STRATA_OWNER: "Strata - Owner",
+};
+
+export const CUSTOMER_TYPE_OPTIONS = Object.values(CUSTOMER_TYPE);
+
+export const ORGANISATION_TYPE = {
+  BUILDER: "Builder",
+  STRATA: "Strata",
+  REAL_ESTATE: "Real Estate",
+  DEVELOPER: "Developer",
+  OTHER: "Other",
+};
+
+export const ORGANISATION_TYPE_OPTIONS = Object.values(ORGANISATION_TYPE);
