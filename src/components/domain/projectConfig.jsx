99d76c0632bb4a1baca4c3223d0a7a9e5a56b9df
admin <admin@kangaroogd.com.au>
2025-12-03 20:52:12 +0000

    File changes

---
+++ src/components/domain/projectConfig.jsx
@@ -0,0 +1,11 @@
+export const PROJECT_TYPE = {
+  GARAGE_DOOR_INSTALL: "Garage Door Install",
+  GATE_INSTALL: "Gate Install",
+  ROLLER_SHUTTER_INSTALL: "Roller Shutter Install",
+  MOTOR_ACCESSORY: "Motor/Accessory",
+  MULTIPLE: "Multiple",
+  REPAIR: "Repair",
+  MAINTENANCE: "Maintenance",
+};
+
+export const PROJECT_TYPE_OPTIONS = Object.values(PROJECT_TYPE);
