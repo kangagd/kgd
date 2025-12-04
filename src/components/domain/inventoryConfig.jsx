89546765e7331fcf9e95eab03735277f8dac1b09
admin <admin@kangaroogd.com.au>
2025-12-04 02:18:17 +0000

    File changes

---
+++ src/components/domain/inventoryConfig.jsx
@@ -0,0 +1,13 @@
+export const LOCATION_TYPE = {
+  WAREHOUSE: "warehouse",
+  VEHICLE: "vehicle",
+  OTHER: "other",
+};
+
+export const MOVEMENT_TYPE = {
+  RESTOCK: "restock",
+  USAGE: "usage",
+  TRANSFER: "transfer",
+  RETURN: "return",
+  ADJUSTMENT: "adjustment",
+};
