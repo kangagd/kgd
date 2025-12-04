f435b7a8af1583aa53f3f6531f37f0a8f601d644
admin <admin@kangaroogd.com.au>
2025-12-04 00:48:08 +0000

    File changes

--- src/components/projects/FinancialsTab.jsx
+++ src/components/projects/FinancialsTab.jsx
@@ -281,6 +281,7 @@ export default function FinancialsTab({ project, onUpdate }) {
                 </div>
               </div>
             </div>
+            </div>
           )}
         </CardContent>
       </Card>
