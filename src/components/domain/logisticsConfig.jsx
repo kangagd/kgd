b7745022db9fb2fec8bde062d74578c2a82807f3
admin <admin@kangaroogd.com.au>
2025-12-10 06:31:54 +0000

    Loading Bay Item Display

--- src/components/domain/logisticsConfig.jsx
+++ src/components/domain/logisticsConfig.jsx
@@ -9,7 +9,7 @@ export const PO_STATUS = {
   SENT: "Sent",
   CONFIRMED: "Confirmed",
   READY_TO_PICK_UP: "Ready to Pick Up",
-  DELIVERED_TO_DELIVERY_BAY: "Delivered to Loading Bay",
+  DELIVERED_TO_DELIVERY_BAY: "Delivered to Delivery Bay",
   COMPLETED_IN_STORAGE: "Completed - In Storage",
   COMPLETED_IN_VEHICLE: "Completed - In Vehicle",
 };
