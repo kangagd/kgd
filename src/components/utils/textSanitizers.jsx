384e5bfc99eed41b2ad87ab6476261e2f28427bc
admin <admin@kangaroogd.com.au>
2025-12-22 04:10:25 +0000

    File changes

---
+++ src/components/utils/textSanitizers.jsx
@@ -0,0 +1,24 @@
+/**
+ * Sanitize inbound text from external sources (emails, forms, etc.)
+ * Handles encoding issues, special characters, and normalizes whitespace
+ */
+export const sanitizeInboundText = (text) => {
+  if (!text) return text;
+
+  return text
+    // Replace HTML non-breaking space entity
+    .replace(/&nbsp;/g, ' ')
+    // Replace narrow no-break space (common encoding issue)
+    .replace(/â¯/g, ' ')
+    // Remove stray â characters (common encoding artifact)
+    .replace(/â/g, '')
+    // Replace Unicode non-breaking space
+    .replace(/\u00A0/g, ' ')
+    // Remove zero-width characters
+    .replace(/[\u200B-\u200D\uFEFF]/g, '')
+    // Normalize curly quotes to straight quotes
+    .replace(/[\u201C\u201D]/g, '"')  // curly double quotes
+    .replace(/[\u2018\u2019]/g, "'")  // curly single quotes
+    // Normalize dashes
+    .replace(/[\u2013\u2014]/g, '-'); // en-dash and em-dash to hyphen
+};
