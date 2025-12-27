1b1f223e5ce1e4d9b7a613778ac580156b753308
admin <admin@kangaroogd.com.au>
2025-12-27 03:48:26 +0000

    File changes

--- functions/managePurchaseOrder.ts
+++ functions/managePurchaseOrder.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.4';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
 import { updateProjectActivity } from './updateProjectActivity.js';
 
 // Canonical PO status values (stored in DB)
