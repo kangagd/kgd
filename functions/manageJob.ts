260e878bfe0e3551b87eac1cbba5326b7b59b860
admin <admin@kangaroogd.com.au>
2025-12-30 23:30:58 +0000

    File changes

--- functions/manageJob.ts
+++ functions/manageJob.ts
@@ -186,25 +186,33 @@ Deno.serve(async (req) => {
                 }
             }
 
+            // Check if this is "Initial Site Visit" job being created from a project at that stage
+            if (jobData.project_id && !jobData.job_type_id) {
+                try {
+                    const project = await base44.asServiceRole.entities.Project.get(jobData.project_id);
+                    if (project && project.status === 'Initial Site Visit') {
+                        // Auto-set job type to "Initial Site Visit"
+                        const jobTypes = await base44.asServiceRole.entities.JobType.filter({ 
+                            name: 'Initial Site Visit' 
+                        });
+                        if (jobTypes.length > 0) {
+                            jobData.job_type_id = jobTypes[0].id;
+                            jobData.job_type = jobTypes[0].name;
+                            jobData.job_type_name = jobTypes[0].name;
+                        }
+                    }
+                } catch (e) {
+                    console.error("Error auto-setting Initial Site Visit job type:", e);
+                }
+            }
+
             // Auto-link contract logic
             if (jobData.customer_id) {
                 try {
                     const customer = await base44.asServiceRole.entities.Customer.get(jobData.customer_id);
                     if (customer && customer.contract_id) {
                         jobData.contract_id = customer.contract_id;
-                        jobData.organisation_id = customer.organisation_id; // Although Job doesn't have organisation_id in schema, user request implies it. Wait, schema check: Customer has org_id. Job instructions say "job.organisation_id = customer.organisation_id". I didn't add organisation_id to Job schema in previous step because I missed that specific line in point 5. Let me re-read. 
-                        // Point 4 "Extend Job": contract_id, sla_due_at, is_contract_job. 
-                        // Point 5 "Auto-link": job.organisation_id = customer.organisation_id. 
-                        // I should probably add organisation_id to Job entity as well to be safe, or just ignore if schema doesn't have it. 
-                        // But I just wrote Job.json without organisation_id. 
-                        // Let's assume I should add it if I can, or just skip if I didn't.
-                        // Actually, it's better to add it to Job schema now if I can, or just proceed without it if it's not critical.
-                        // User explicit instruction in point 5 override point 4?
-                        // "Extend Job to support contracts + SLA" didn't list organisation_id.
-                        // But "Auto-link behaviour" lists it.
-                        // I'll add it to the data, if schema allows additional properties or I'll update schema quickly.
-                        // Nah, I'll just skip organisation_id for Job for now to stick to schema I just wrote, or I can update schema later.
-                        // Actually, let's just add contract info.
+                        jobData.organisation_id = customer.organisation_id;
                         
                         jobData.is_contract_job = true;
 
