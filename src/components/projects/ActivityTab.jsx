63fc487ee36541f2c037c2bb3c20e3d355ff2fc3
admin <admin@kangaroogd.com.au>
2026-01-03 09:51:00 +0000

    File changes

--- src/components/projects/ActivityTab.jsx
+++ src/components/projects/ActivityTab.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useRef, useEffect } from "react";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { base44 } from "@/api/base44Client";
@@ -549,7 +548,7 @@ function EmailThreadViewerModal({
     onComposeEmail?.({
       mode: 'reply',
       thread: threadFromList,
-      message: { ...latestMessage, message_id: latestMessage.gmail_message_id }
+      message: latestMessage
     });
   };
 
@@ -665,7 +664,7 @@ function EmailThreadViewerModal({
                         onComposeEmail?.({
                           mode: 'reply',
                           thread: threadFromList,
-                          message: { ...message, message_id: message.gmail_message_id }
+                          message: message
                         });
                       }}
                       onForward={(message) => {
