8c050abb2fefbecd5a5816133dd024800d03f611
admin <admin@kangaroogd.com.au>
2026-01-08 04:04:50 +0000

    File changes

--- functions/managePart.ts
+++ functions/managePart.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.4';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
 import { updateProjectActivity } from './updateProjectActivity.js';
 import { LOGISTICS_PURPOSE } from './shared/constants.js';
 
