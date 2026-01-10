459062f4acd199c3cd64b2063f962b0427620699
admin <admin@kangaroogd.com.au>
2026-01-10 22:30:56 +0000

    File changes

--- functions/linkOrphanedXeroInvoices.ts
+++ functions/linkOrphanedXeroInvoices.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from './shared/sdk.js';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
 
 // Admin-only function to link orphaned XeroInvoices to their projects
 Deno.serve(async (req) => {
