d0ab283ecbfefdc7c52d3439a0eb4ff9bf2eabfa
admin <admin@kangaroogd.com.au>
2026-01-10 10:30:47 +0000

    File changes

--- src/pages/Inbox.jsx
+++ src/pages/Inbox.jsx
@@ -197,14 +197,33 @@ export default function Inbox() {
               {/* Messages & Composer */}
               <div className="flex-1 overflow-y-auto min-h-0 flex flex-col">
                 <div className="flex-1 overflow-y-auto p-4">
-                  <EmailDetailView
-                    thread={selectedThread}
-                    onReply={(message, thread) => {
-                      setComposerMessage({ message, thread });
-                      setShowComposer(true);
-                    }}
-                  />
-                </div>
+                   <EmailDetailView
+                     thread={selectedThread}
+                     userPermissions={{
+                       can_reply: true,
+                       can_change_status: true,
+                       can_link_to_project: true,
+                       can_create_project_from_email: true,
+                       can_create_job_from_email: true
+                     }}
+                     onClose={() => setSelectedThreadId(null)}
+                     onLinkProject={() => {}}
+                     onLinkJob={() => {}}
+                     onUnlinkProject={() => {}}
+                     onUnlinkJob={() => {}}
+                     onDelete={async (threadId) => {
+                       try {
+                         await base44.entities.EmailThread.update(threadId, { is_deleted: true });
+                         await refetchThreads();
+                         setSelectedThreadId(null);
+                         toast.success('Thread deleted');
+                       } catch (error) {
+                         toast.error('Failed to delete thread');
+                       }
+                     }}
+                     onThreadUpdate={refetchThreads}
+                   />
+                 </div>
 
                 {/* Composer Section */}
                 {showComposer && (
