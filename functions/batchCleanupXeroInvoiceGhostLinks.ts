b3e2e4d4e543222561f2399ebec5712784ffff9d
admin <admin@kangaroogd.com.au>
2026-01-10 22:30:16 +0000

    File changes

--- functions/batchCleanupXeroInvoiceGhostLinks.ts
+++ functions/batchCleanupXeroInvoiceGhostLinks.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from './shared/sdk.js';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
 
 // Admin-only batch cleanup function to fix ghost invoice links
 Deno.serve(async (req) => {
