9c6d3b10e0ae022e90f02fb07ede2ac041e8ba10
admin <admin@kangaroogd.com.au>
2026-01-16 23:12:40 +0000

    File changes

--- src/components/api/projectKeys.jsx
+++ src/components/api/projectKeys.jsx
@@ -1,6 +1,7 @@
+
 export const projectKeys = {
   all: () => ["projects"],
   list: () => ["projects", "list"],
   detail: (projectId) => ["projects", "detail", projectId],
-  withRelations: (projectId) => ["projects", "withRelations", projectId],
+  withRelations: (projectId, tab) => ["projects", "withRelations", projectId, tab || "all"],
 };
