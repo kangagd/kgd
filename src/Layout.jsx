ba38002e43080bcbc63b9d750d4324b138a0bd42
admin <admin@kangaroogd.com.au>
2026-01-17 10:52:56 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -44,6 +44,7 @@ import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover
 import GlobalSearchDropdown from "./components/common/GlobalSearchDropdown";
 import { RoleBadge, PermissionsProvider } from "./components/common/PermissionsContext";
 import NotificationBell from "./components/notifications/NotificationBell";
+import EnvironmentBanner from "./components/common/EnvironmentBanner";
 import CommandPalette from "@/components/common/CommandPalette";
 import ActiveCheckInBanner from "@/components/common/ActiveCheckInBanner";
 import PullToRefresh from "@/components/common/PullToRefresh";
