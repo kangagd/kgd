7c5c9382b88556bbbfe60e4ba8ef193f1cb80919
admin <admin@kangaroogd.com.au>
2026-01-19 03:37:58 +0000

    File changes

--- functions/recordLogisticsJobTransfer.ts
+++ functions/recordLogisticsJobTransfer.ts
@@ -1,4 +1,5 @@
 import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
+import { getOrCreateSupplierInventoryLocation } from './shared/supplierLocationHelper.js';
 
 Deno.serve(async (req) => {
   try {
