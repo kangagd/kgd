f69c56152dc8b4fbcf3a3ec99dea618278df9103
admin <admin@kangaroogd.com.au>
2026-01-29 08:02:51 +0000

    File changes

--- src/pages/InboxV2.jsx
+++ src/pages/InboxV2.jsx
@@ -1721,6 +1721,7 @@ Link: ${threadLink}
             <InboxV2ContextPanel
               thread={selectedThread}
               teamUsers={teamUsers}
+              currentUser={user}
               onThreadUpdate={() => refetchThreads()}
               onOpenLinkModal={() => setShowLinkModal(true)}
               onOpenCreateProjectModal={() => setShowCreateProjectModal(true)}
